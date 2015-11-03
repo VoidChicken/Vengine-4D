b3bf602910c296565a4ebd1502929910f079cb86
VoidChicken <chicken.shasal@gmail.com>
2015-11-02 16:46:57 -0800

    Added Renderer class and project.

---
+++ Vengine/Vengine-Win/Renderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vengine_Win
+{
+    enum RendererTypes
+    {
+        OpenGL, DirectX
+    }
+    interface IRenderer
+    {
+        
+    }
+    class Renderer : IRenderer
+    {
+
+        public Renderer GetRenderer(RendererTypes r)
+        {
+            if (r == RendererTypes.DirectX)
+            {
+                return new DirectXRenderer();
+            }
+            else if (r == RendererTypes.OpenGL)
+            {
+                return new OpenGLRenderer();
+            }
+            return new Renderer();
+        }
+    }
+    class OpenGLRenderer : Renderer,IRenderer
+    {
+
+    }           
+    class DirectXRenderer : Renderer,IRenderer
+    {
+
+    }
+}
